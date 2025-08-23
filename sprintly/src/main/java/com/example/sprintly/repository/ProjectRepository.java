58076fa92ab2e6f8e43b5e5c8c11ce7307285ba9
snowbird <zbyszkonatkanski@gmail.com>
2025-08-23 15:20:22 +0200

    SPRING-2: formatowanie

--- sprintly/src/main/java/com/example/sprintly/repository/ProjectRepository.java
+++ sprintly/src/main/java/com/example/sprintly/repository/ProjectRepository.java
@@ -1,8 +1,4 @@
 package com.example.sprintly.repository;
 
-import com.example.sprintly.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 public interface ProjectRepository {
 }
